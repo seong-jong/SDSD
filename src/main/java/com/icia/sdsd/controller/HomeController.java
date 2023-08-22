cffa2b7ab74cbf904a7c07171cbc67b49220458c
jongs <tjdwhd97@naver.com>
2023-08-22 09:46:18 +0900

    숙덕숙덕

---
+++ src/main/java/com/icia/sdsd/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.icia.sdsd.controller;
+
+import com.icia.sdsd.service.KakaoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+
+}
